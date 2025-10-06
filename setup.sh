7dff9a3252cd19c5d1323bcb8d34395b1c7fb9b6
ozaki <zakioyoyoyo103@gmail.com>
2025-10-07 06:59:08 +0900

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ git submodule update --init --recursive
 
 echo "--- Applying patches to 'spike' submodule... ---"
 # `spike`ディレクトリに対して、`patches`ディレクトリ内の全てのパッチファイルを適用する
-git -C spike apply ../patches/*.patch
+# git -C spike apply ../patches/*.patch
+git -C spike apply ../patches/0001-my-custom-modifications.patch
 
 echo "--- Setup complete! You can now build the project. ---"
